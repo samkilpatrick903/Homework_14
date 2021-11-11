e0b91ace907b1d7ed92712072bc35a2f3755caf5
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-11-10 20:28:24 -0600

    models

---
+++ models/Comment.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    comment_text: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: DataTypes.NOW,
+    },  
+  },
+  
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
---
+++ models/Post.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        name: {
+            type: DataTypes.STRING,
+          },
+          post_content: {
+            type: DataTypes.STRING,
+          },
+          date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+          },
+    },
+{
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'post',
+}
+);
+
+module.exports = Post;
