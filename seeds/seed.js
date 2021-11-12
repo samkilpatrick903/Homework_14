63a7d38c042c19a34c771395512233b4cf46d426
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-11-11 21:04:58 -0600

    seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,30 +1,23 @@
-const express = require('express');
-const session = require('express-session');
-const routes = require('./controllers');
+const sequelize = require("../config/connection");
+const { User, Post, Comment } = require("../models");
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const userData = require("./userData.json");
+const postData = require("./postData.json");
+const commentData = require("./commentData.json");
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Post.bulkCreate(postData);
+
+  await Comment.bulkCreate(commentData);
+
+  process.exit(0);
 };
 
-app.use(session(sess));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+seedDatabase();
