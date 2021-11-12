adfcaf0514c2964d8121014e91e0732609ef4921
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-11-11 20:23:55 -0600

    Brought li lo js from mini project

---
+++ public/js/comment.js
@@ -0,0 +1,27 @@
+const newCommentHandler = async (event) => {
+    event.preventDefault();
+  
+    const comment_text = document.querySelector('#comment').value.trim();
+
+    const post_id = document.getElementById('postid').innerHTML;
+    
+    if (comment_text) {
+      const response = await fetch(`/api/comments`, {
+        method: 'POST',
+        body: JSON.stringify({ comment_text, post_id }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to create comment');
+      }
+    }
+  };
+
+document
+  .querySelector('.comment-form')
+  .addEventListener('submit', newCommentHandler);
