6b6c61449a84de2e941de28f1feb77b2a79b0bfa
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-11-12 14:53:29 -0600

    route change

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+// const { User } = require("../../models");
+const user = require("../../models/user");
+
 
 router.post("/", async (req, res) => {
   try {
