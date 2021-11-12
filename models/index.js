51afd326143922a504327c98f68d99465660f71e
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-11-12 15:01:34 -0600

    adding js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require('./user');
+const User = require('./user.js');
 const Post = require('./post.js');
-const Comment = require('./comment');
+const Comment = require('./comment.js');
 
 Post.belongsTo(User, {
     foreignKey: 'user_id',
