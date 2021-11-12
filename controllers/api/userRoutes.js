54b211623feb3c834a7c5a639af8b929edbb54ff
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-11-12 14:57:56 -0600

    fixed user route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 // const { User } = require("../../models");
-const user = require("../../models/user");
+const User = require("../../models/user");
 
 
 router.post("/", async (req, res) => {
